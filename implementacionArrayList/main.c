48497ec18dc3365a27c6a693ee73fe6e4a67c648
LeonelERey <leogonzalez.17.98@gmail.com>
2017-11-20 22:27:31 -0300

    Add files via upload

--- implementacionArrayList/main.c
+++ implementacionArrayList/main.c
@@ -7,7 +7,9 @@
 int main()
 {
     ArrayList* lista;
+    ArrayList* lista2;
     xPersona* persona;
+    xPersona* persona2;
     FILE* archivo;
     FILE* archivoCopia;
     lista=al_newArrayList();
@@ -25,7 +27,7 @@ int main()
         printf("5- Agregar persona en posicion especificada.\n");
         printf("6- Ordenar personas.\n");
         printf("7- Mostrar personas.\n");
-        printf("8- Eliminar todas las personas.\n");
+        printf("8- Mostar desde hasta.\n");
         printf("9- Crear copia de seguridad.\n");
         printf("10- Salir.\n");
         printf("Opcion:");
@@ -55,7 +57,9 @@ int main()
             mostrarPersona(lista,persona);
             break;
         case 8:
-            lista->clear(lista);
+            lista2=lista->subList(lista,89,99);
+            mostrarPersona(lista2,persona);
+            //lista->clear(lista);
             break;
         case 9:
             printf("desea hacer una copia de seguridad? s/n:");
